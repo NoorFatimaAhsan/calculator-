b29030abff1f0a4231e9bae7159f54a7db02c7c5
NoorFatimaAhsan <noorfatimahsan@gmail.com>
2024-11-13 21:21:16 +0500

    first commit

---
+++ calculator.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+#include<conio.h>
+int main(){
+    int number1,number2,option;
+    printf("Enter 2 numbers: ");
+    scanf("%d%d",&number1,&number2);
+    printf("Press 1:addition \n Press 2:substraction \n Press 3:multiplication \nPress 4: division\n");
+    scanf("%d",&option);
+    getche();
+    }
