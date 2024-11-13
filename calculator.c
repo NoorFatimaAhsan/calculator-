b3fa5e25f2303eadd04bafa72969f64df6d18987
NoorFatimaAhsan <noorfatimahsan@gmail.com>
2024-11-13 21:24:42 +0500

    addition feature added

--- calculator.c
+++ calculator.c
@@ -6,5 +6,11 @@ int main(){
     scanf("%d%d",&number1,&number2);
     printf("Press 1:addition \n Press 2:substraction \n Press 3:multiplication \nPress 4: division\n");
     scanf("%d",&option);
+
+    switch(option)
+    {case 1 :
+    printf("addition of %d and %d: %d",number1,number2,number1+number2);
+    break;
+    }
     getche();
     }
